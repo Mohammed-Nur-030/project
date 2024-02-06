00436e740a6a174cd12a25ddb1116afc6a7bc089
nanipk17 <142439859+nanipk17@users.noreply.github.com>
2024-02-06 16:30:36 +0530

    Update index.jsx

--- components/chat/index.jsx
+++ components/chat/index.jsx
@@ -9,7 +9,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 import useAudioRecorder from '../../utils/recordAudio'
 
-const base = 'http://chatbot-backend-beta.vercel.app'
+const base = 'https://chatbot-backend-beta.vercel.app'
 
 const ChatWidget = ({ siteURL, lang, apiKey, mail }) => {
 
